431de3534fc5e4a73d504c7e7ec3b1886bf96b38
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-04-15 20:14:44 -0400

    Webpack for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
@@ -39,5 +40,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "styles.css",
     }),
+    CleanWebpackPlugin()
   ],
 };
